bbc33cf4bb0eebc45f52b39606b5ad7016bf4ca5
niallwphd <niallw@terpmail.umd.edu>
2021-05-11 00:00:18 -0400

    basics of timers done

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
-function testTimerFunction(numMinutesCountdown) {
+var x;
+var timerStarted = false;
+var unboundedTimerStartTime;
+
+function timerFunction(numMinutesCountdown) {
+    console.log("Timer started: " + timerStarted);
+    console.log("==========");
+
+    if (timerStarted){
+        console.log("Timer still running!");
+        return;
+    }
+
+    timerStarted = true;
+
     // Set the date we're counting down to
     var endTime = new Date().getTime() + (numMinutesCountdown * 60000);
 
     // Update the count down every 1 second
-    var x = setInterval(function() {
+    x = setInterval(function() {
 
     // Get today's date and time
     var now = new Date().getTime();
@@ -18,7 +32,7 @@ function testTimerFunction(numMinutesCountdown) {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
         
     // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    document.getElementById("demo").innerHTML = "Time remaining: " + days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
         
     // If the count down is over, write some text 
@@ -27,7 +41,50 @@ function testTimerFunction(numMinutesCountdown) {
         var audio = new Audio('js/alarm.mp3');
         audio.play();
         document.getElementById("demo").innerHTML = "EXPIRED";
+        timerStarted = false;
     }
 
     }, 1000);
+}
+
+function customTimer(){
+    var customTime = parseFloat(document.getElementById("customTime").value);
+    timerFunction(customTime);   
+}
+
+function unboundedTimer(){
+    if (timerStarted){
+        console.log("Timer still running!");
+        return;
+    }
+
+    if (!timerStarted){
+        unboundedTimerStartTime = new Date().getTime();
+        timerStarted = true;
+    }
+
+    x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+        
+    // Find the distance between now and the count down date
+    var distance = now - unboundedTimerStartTime;
+        
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+    // Output the result in an element with id="demo"
+    document.getElementById("demo").innerHTML = "Unbounded session length: " + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";        
+    }, 1000);
+}
+
+function stopTimer(){
+    timerStarted = false;
+    document.getElementById("demo").innerHTML = "No timer running.";
+    clearInterval(x);
 }
