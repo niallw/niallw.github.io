2c9768e92be76b0c54aa19d82fb3cd23ac96b804
niallwphd <niallw@terpmail.umd.edu>
2021-05-24 10:58:46 -0400

    add duration logger

--- js/scripts.js
+++ js/scripts.js
@@ -230,7 +230,7 @@ function logSession() {
     console.log("Updating logs...");
 
     var _values = [
-        [new Date(startTime).toString(), new Date(endTimeToRecord).toString(), sessionLengthToRecord / 60]    
+        [new Date(startTime).toString(), new Date(endTimeToRecord).toString(), sessionLengthToRecord]    
     ];
     
     var body = {
