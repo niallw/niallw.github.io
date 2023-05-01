7f8313f7aaccdd2f8cad018f198f395d0579ce1d
Niall L. Williams <niallw@terpmail.umd.edu>
2023-05-01 03:57:28 -0400

    fun pics and removed files folder

---
+++ js/img_hover.js
@@ -0,0 +1,43 @@
+// var arr = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg"];
+
+// function getRandomImage() {
+//   var index = Math.floor(Math.random() * arr.length);
+//   return arr[index];
+// }
+
+// $("#div").hover(
+//   function() {
+//     var image = getRandomImage();
+//     $("#img").attr("src", image);
+//     console.log(image);
+//   });
+
+var images = [
+    "img/profile-fun1.png",
+    "img/profile-fun2.png",
+    "img/profile-fun3.png",
+    "img/profile-fun4.png",
+    "img/profile-fun5.png",
+    "img/profile-fun6.png",
+    "img/profile-fun7.png"
+];
+
+function getRandomImage(event) {
+    var index = Math.floor(Math.random() * images.length);
+    $("#me").attr("src", images[index]);
+
+    // return images[index];
+}
+
+function setToDefault(){
+    console.log("to default");
+    $("#me").attr("src", "img/profile.png");
+}
+
+$("#profile-div").hover(
+function() {
+    var image = getRandomImage();
+    $("#img").attr("src", image);
+    // $("#me").attr("src", image);
+    console.log(image);
+});
